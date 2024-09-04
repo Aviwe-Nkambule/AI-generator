f1391cf3e4609f435452a3e96946d477a3e09323
Aviwe-Nkambule <nkambuleaviwe@gmail.com>
2024-09-04 13:47:15 +0200

    Added AI API intergration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
-function generateRecipe(event) {
-  event.preventDefault();
-
+function displayRecipe(response) {
+  console.log("recipe generated");
   new Typewriter("#recipe", {
-    strings:
-      "Similar to the British custard tart or Portuguese pasteis de nata, melktert consists of a pastry case filled with milk, eggs and which is usually thickened with flour. The finished tart is traditionally dusted with cinnamon.",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateRecipe(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "c53cf67ebfdea328dfb55e410tc594od";
+  let context =
+    "be polite and provide a very short recipe.Your mission is to generate a 4 line recipe in basic HTML element and seperate each line with a <br />. Make sure to follow the user instructions.Do not include a title to the recipe. Sign the recipe with 'SheCodes AI'inside <strong> element at the bottom of the recipe and NOT at the beginning";
+  let prompt = `User instructions: Generate best recipes in ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating recipe");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeFormElement = document.querySelector("#recipe-generator-form");

8dd024f8f23f044d20d1e9ac76859e8e94987ee9
Aviwe-Nkambule <nkambuleaviwe@gmail.com>
2024-09-04 14:27:52 +0200

    Improved the User Experience

--- src/index.js
+++ src/index.js
@@ -18,6 +18,10 @@ function generateRecipe(event) {
   let prompt = `User instructions: Generate best recipes in ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.classList.remove("hidden");
+  recipeElement.innerHTML = `<div class="generating">⌛Generating the recipe for ${instructionsInput.value}</div>`;
+
   console.log("Generating recipe");
   console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${context}`);
